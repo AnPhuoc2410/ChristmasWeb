4c9d7670b2c45f6967a7867fe51a61dc0156d0f9
Glitch (glitch-hello-website) <none>
2023-11-29 15:42:58 +0000

    📅📕 Checkpoint ./style.css:63284340/57 ./index.html:63284340/2675 ./script.js:63284340/1958

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+        // Tạo tuyết rơi
+        function createSnowflake() {
+            const snowflake = document.createElement('div');
+            snowflake.className = 'snowflake';
+            snowflake.innerHTML = '❄';
+            snowflake.style.left = Math.random() * window.innerWidth + 'px';
+            document.body.appendChild(snowflake);
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) {
-  // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
+            // Khi tuyết rơi đến dưới cùng, hủy nó và tạo một cái mới
+            snowflake.addEventListener('animationiteration', () => {
+                snowflake.style.left = Math.random() * window.innerWidth + 'px';
+            });
 
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
+            return snowflake;
+        }
 
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } },
-    "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => {
-    goto(f.dataset.file, f.dataset.line);
-  };
-});
+        // Tạo 50 tuyết rơi ban đầu
+        for (let i = 0; i < 50; i++) {
+            createSnowflake();
+        }
+    
